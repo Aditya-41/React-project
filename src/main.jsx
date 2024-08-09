3370e32d04a815ac7981663083b8088784472d5d
Aditya-41 <adityapatil0226@gmail.com>
2024-08-09 14:33:45 +0530

    configure ract hot toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-
-
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import './index.css';
 
 
 import { createRoot } from 'react-dom/client';
+
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
      <App />
+     <Toaster/>
   </BrowserRouter>
   
   
