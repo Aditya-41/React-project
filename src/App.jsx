87ee5189fa4b9fdb548ee114e78bb92ee8a58257
Aditya-41 <adityapatil0226@gmail.com>
2024-08-10 20:24:27 +0530

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 
 import AboutUs from './Pages/AboutUs'
 import HomePage from './Pages/HomePage'
+import NotFound from './Pages/NotFound'
 
 
 
@@ -17,6 +18,9 @@ function App() {
       <Routes>
         <Route path='/' element = { <HomePage /> }></Route>
         <Route path='/about' element={<AboutUs />}></Route>
+
+
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes> 
     </>
   )
